c83a5bd856b045d88333706b6b0aae2f78056542
dramirezr <diego_ramirez_romero@hotmail.com>
2014-05-13 10:46:53 -0500

    identificar mobil

--- assets/js/mochkino.js
+++ assets/js/mochkino.js
@@ -27,6 +27,18 @@ var isTouch = false;
 	});	
 }*/
 
+var device = navigator.userAgent;
+var browsermobile = false;
+
+if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+{ 
+    browsermobile = true;
+}
+
+
+
+
+
 var directionsService = new google.maps.DirectionsService();
 
 var styles = [
@@ -832,7 +844,9 @@ $(document).ready(function() {
     });
     
     if(!isTouch){
-    	$('input[name="search-text"]').focus();
+        if(!browsermobile){
+    	   $('input[name="search-text"]').focus();
+        }
     }
     
     $('input[name="search-text"]').keydown(function(e) {
